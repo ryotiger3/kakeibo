770a00a503196ac1345002dd7139b121870b45ba
ryotiger3 <rothsryo319@gmail.com>
2021-10-06 22:30:57 +0900

    収入金額機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_112255) do
+ActiveRecord::Schema.define(version: 2021_10_06_131809) do
 
   create_table "fixedcosts", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "income_values", charset: "utf8mb4", force: :cascade do |t|
+    t.integer "income_id", null: false
+    t.date "year_month"
+    t.integer "value"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
